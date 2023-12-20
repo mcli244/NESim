7a6eb88004f11fa011970b4c34672889c23a0cba
lixiangjun <lixiangjun@abupdate.com>
2023-12-20 14:56:27 +0800

    [feat] test cpu

--- src/olc6502.cpp
+++ src/olc6502.cpp
@@ -46,6 +46,11 @@ namespace nes
         return cpuBus.read(addr);
     }
 
+    uint8_t olc6502::read(uint16_t addr, bool s)
+    {
+        return cpuBus.read(addr);
+    }
+
     void olc6502::write(uint16_t addr, uint8_t value)
     {
         cpuBus.write(addr, value);
@@ -58,7 +63,7 @@ namespace nes
         uint16_t hi = read(addr_tmp + 1);
 
         pc = (hi << 8) | lo;
-        a = y = x = 0;
+        a = y = x = status = 0;
         stkp = 0xFD;
 
         cycles = 8; // 复位动作需要8个时钟周期，实际这里模拟应该不需要，硬件复位是一个操作然后等待8个时钟后完成以上动作，这里先保留这个时钟节拍。
@@ -1257,7 +1262,143 @@ namespace nes
         return 0;
     }
 
+    ///////////////////////////////////////////////////////////////////////////////
+    // HELPER FUNCTIONS
 
+    bool olc6502::complete()
+    {
+        return cycles == 0;
+    }
 
+    bool olc6502::pass()
+    {
+        cycles = 0;
+        return true;
+    }
+
+    // This is the disassembly function. Its workings are not required for emulation.
+    // It is merely a convenience function to turn the binary instruction code into
+    // human readable form. Its included as part of the emulator because it can take
+    // advantage of many of the CPUs internal operations to do this.
+    std::map<uint16_t, std::string> olc6502::disassemble(uint16_t nStart, uint16_t nStop)
+    {
+        uint32_t addr = nStart;
+        uint8_t value = 0x00, lo = 0x00, hi = 0x00;
+        std::map<uint16_t, std::string> mapLines;
+        uint16_t line_addr = 0;
+
+        // A convenient utility to convert variables into
+        // hex strings because "modern C++"'s method with 
+        // streams is atrocious
+        auto hex = [](uint32_t n, uint8_t d)
+        {
+            std::string s(d, '0');
+            for (int i = d - 1; i >= 0; i--, n >>= 4)
+                s[i] = "0123456789ABCDEF"[n & 0xF];
+            return s;
+        };
+
+        // Starting at the specified address we read an instruction
+        // byte, which in turn yields information from the lookup table
+        // as to how many additional bytes we need to read and what the
+        // addressing mode is. I need this info to assemble human readable
+        // syntax, which is different depending upon the addressing mode
+
+        // As the instruction is decoded, a std::string is assembled
+        // with the readable output
+        while (addr <= (uint32_t)nStop)
+        {
+            line_addr = addr;
+
+            // Prefix line with instruction address
+            std::string sInst = "$" + hex(addr, 4) + ": ";
+
+            // Read instruction, and get its readable name
+            uint8_t opcode = read(addr, true); addr++;
+            sInst += lookup[opcode].name + " ";
+
+            // Get oprands from desired locations, and form the
+            // instruction based upon its addressing mode. These
+            // routines mimmick the actual fetch routine of the
+            // 6502 in order to get accurate data as part of the
+            // instruction
+            if (lookup[opcode].addrmode == &olc6502::IMP)
+            {
+                sInst += " {IMP}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::IMM)
+            {
+                value = read(addr, true); addr++;
+                sInst += "#$" + hex(value, 2) + " {IMM}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::ZP0)
+            {
+                lo = read(addr, true); addr++;
+                hi = 0x00;												
+                sInst += "$" + hex(lo, 2) + " {ZP0}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::ZPX)
+            {
+                lo = read(addr, true); addr++;
+                hi = 0x00;														
+                sInst += "$" + hex(lo, 2) + ", X {ZPX}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::ZPY)
+            {
+                lo = read(addr, true); addr++;
+                hi = 0x00;														
+                sInst += "$" + hex(lo, 2) + ", Y {ZPY}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::IZX)
+            {
+                lo = read(addr, true); addr++;
+                hi = 0x00;								
+                sInst += "($" + hex(lo, 2) + ", X) {IZX}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::IZY)
+            {
+                lo = read(addr, true); addr++;
+                hi = 0x00;								
+                sInst += "($" + hex(lo, 2) + "), Y {IZY}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::ABS)
+            {
+                lo = read(addr, true); addr++;
+                hi = read(addr, true); addr++;
+                sInst += "$" + hex((uint16_t)(hi << 8) | lo, 4) + " {ABS}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::ABX)
+            {
+                lo = read(addr, true); addr++;
+                hi = read(addr, true); addr++;
+                sInst += "$" + hex((uint16_t)(hi << 8) | lo, 4) + ", X {ABX}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::ABY)
+            {
+                lo = read(addr, true); addr++;
+                hi = read(addr, true); addr++;
+                sInst += "$" + hex((uint16_t)(hi << 8) | lo, 4) + ", Y {ABY}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::IND)
+            {
+                lo = read(addr, true); addr++;
+                hi = read(addr, true); addr++;
+                sInst += "($" + hex((uint16_t)(hi << 8) | lo, 4) + ") {IND}";
+            }
+            else if (lookup[opcode].addrmode == &olc6502::REL)
+            {
+                value = read(addr, true); addr++;
+                sInst += "$" + hex(value, 2) + " [$" + hex(addr + value, 4) + "] {REL}";
+            }
+
+            // Add the formed string to a std::map, using the instruction's
+            // address as the key. This makes it convenient to look for later
+            // as the instructions are variable in length, so a straight up
+            // incremental index is not sufficient.
+            mapLines[line_addr] = sInst;
+        }
+
+        return mapLines;
+    }
 }
 
